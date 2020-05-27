e5cebee0d37c191dabee4f9052b2fca90260d9a2
Wouter <wouterfolkertsma@gmail.com>
2020-05-27 18:11:35 +0200

    Readded jwt refresh middleware to api routes

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::group(['middleware' => 'api'], function () {
     | Authentication required
     |--------------------------------------------------------------------------
     */
-    Route::group(['middleware' => ['auth:api']], function () {
+    Route::group(['middleware' => ['auth:api', 'jwt.refresh']], function () {
 
         /** Courses */
         Route::get('courses/all', 'CourseController@all')->name('courses.all');
