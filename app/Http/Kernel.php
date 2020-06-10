b468a5138d60542298000c573e174ca341e84c95
Roy Voetman <royvoetman@outlook.com>
2020-06-10 16:16:09 +0200

    Added middleware to create update and destroy routes.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'auth.refresh' => \App\Http\Middleware\ValidRefreshToken::class,
-        'auth.admin' => \App\Http\Middleware\Admin::class,
+        'auth.staff' => \App\Http\Middleware\IsStaff::class,
     ];
 }
